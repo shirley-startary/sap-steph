9584eb253558715810bd95ea9e48e6365c387ee1
Ana Steph <anastephany.salazarguzman@gmail.com>
2020-02-11 01:34:42 -0600

    feat(LatinCodeWeek): Add carousel (#9)

--- pages/latin-code-week.jsx
+++ pages/latin-code-week.jsx
@@ -1,60 +1,150 @@
 import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
 import NavigationBar from '../components/navigationBar';
 import RectangularCard from '../components/rectangularCard';
+import dataProyectos from '../data/latinCodeWeek';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const tutorialSteps = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath:
+      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bird',
+    imgPath:
+      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:
+      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+  },
+  {
+    label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
+    imgPath:
+      'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Goč, Serbia',
+    imgPath:
+      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+];
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    maxWidth: 400,
+    flexGrow: 1,
+    paddingTop: '110px',
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    height: 50,
+    paddingLeft: theme.spacing(4),
+    backgroundColor: theme.palette.background.default,
+  },
+  img: {
+    height: 255,
+    display: 'block',
+    maxWidth: 400,
+    overflow: 'hidden',
+    width: '100%',
+  },
+}));
 
 const LatinCodeWeek = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = tutorialSteps.length;
 
-  const dataProyectos = [
-    {
-      'title':'Fomentar el ahorro',
-      'subtitulo':'One family:Aplicación familiar donde se plantean una meta de ahorro',
-      'thumbnail':'',
-      'Objective':['Objetivo 8 Trabajo decente y crecimiento económico'],
-    },
-    {
-      'title':'Mejorar la Calidad de la Educación en México',
-      'subtitulo':'Uni App: aplicación que brinda opciones de educación universitaria con becas, programas e intercambios.',
-      'thumbnail':'',
-      'Objective':['Objetivo 4 Educación de Calidad'],
-    },
-    {
-      'title':'Ciudades inteligentes',
-      'subtitulo':'Girly Protect: pulsera para la seguridad de mujeres. ',
-      'thumbnail':'',
-      'Objective':['Objetivo 3 Salud y Bienestar ','Objetivo 9 Industria, Innovación e Infraestructura'],
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
 
-    },
-    {
-      'title':'Deserción escolar',
-      'subtitulo':'Security way: transporte seguro y accesible para traslado de estudiantes.',
-      'thumbnail':'',
-      'Objective':['Objetivo 11 Ciudades y Comunidades Sostenibles','Objetivo 4 Educación de Calidad'],
-    },
-  ]
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
 
-  return (<div>
-    <NavigationBar />
-    <div className="">
-      <img src="https://dummyimage.com/vga" className="imagePresentation" alt="dummy"/>
-      <div>
-        <p>
-          Latin Code Week acerca a los jóvenes a las carreras de ciencia, tecnología, ingeniería y matemáticas (STEM), 
-          para convertirse en la fuerza laboral de los empleos del futuro. Más de 5,000 jóvenes han sido impactados gracias a este programa.        </p>
-      </div>
-      <section>
-        {dataProyectos.map(item => (
-          <RectangularCard  
-          thumbnail=''
-          title={item.title}
-          subtitle={item.subtitulo}
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <div>
+      <NavigationBar />
+      <div className={classes.root}>
+        <Paper square elevation={0} className={classes.header}>
+          <Typography>{tutorialSteps[activeStep].label}</Typography>
+        </Paper>
+        <AutoPlaySwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={activeStep}
+          onChangeIndex={handleStepChange}
+          enableMouseEvents
+        >
+          {tutorialSteps.map((step, index) => (
+            <div key={step.label}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <img className={classes.img} src={step.imgPath} alt={step.label} />
+              ) : null}
+            </div>
+          ))}
+        </AutoPlaySwipeableViews>
+        <MobileStepper
+          steps={maxSteps}
+          position="static"
+          variant="text"
+          activeStep={activeStep}
+          nextButton={(
+            <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+              Next
+              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </Button>
+          )}
+          backButton={(
+            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            Back
+            </Button>
+        )}
         />
-        ))}
-        
-      </section>
-    </div>
+      </div>
+      <div className="">
+        <img src="https://dummyimage.com/vga" className="imagePresentation" alt="dummy" />
+        <div>
+          <p>
+            Latin Code Week acerca a los jóvenes a las carreras de ciencia, tecnología,
+            ingeniería y matemáticas (STEM), para convertirse en la fuerza laboral de los
+            empleos del futuro. Más de 5,000 jóvenes han sido impactados gracias a este programa.
+          </p>
+        </div>
+        <section>
+          {dataProyectos.map(item => (
+            <RectangularCard
+              thumbnail=""
+              title={item.title}
+              subtitle={item.subtitulo}
+            />
+          ))}
+        </section>
+      </div>
 
-    <style jsx global>
-      {`
+      <style jsx global>
+        {`
         body {
           margin: 0;
           font-family: system-ui;
@@ -68,9 +158,8 @@ const LatinCodeWeek = () => {
         }
         
       `}
-    </style>
-  </div>)
+      </style>
+    </div>
+  );
 };
-
-
 export default LatinCodeWeek;
