437294e941fcb3054a1fff2405792b53df4dae38
shirley <shirley.startary@gmail.com>
2020-02-12 17:51:39 -0600

    add code correction

--- components/carousel.jsx
+++ components/carousel.jsx
@@ -78,6 +78,24 @@ const Carousel = (props) => {
         ))}
       </AutoPlaySwipeableViews>
       <MobileStepper
+        steps={maxSteps}	
+        position="static"	
+        variant="text"	
+        activeStep={activeStep}	
+        nextButton={(	
+          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>	
+            Next	
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}	
+          </Button>	
+        )}	
+        backButton={(	
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>	
+            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}	
+          Back	
+          </Button>	
+        )}	
+      />	
+
       <style jsx>
         {`
         #carousel {
