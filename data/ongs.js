ee7d89b2819e459ed9a9ea54e6a6e152e853bab2
Stephany Salazar <anastephany.salazarguzman@gmail.com>
2020-02-12 23:05:43 -0600

    load(ongs): loading data of ongs

--- data/ongs.js
+++ data/ongs.js
@@ -1,29 +1,26 @@
 export default [
   {
-    title: '',
-    info: '',
+    name: 'Junior Achievement',
+    description: 'Inspiramos y preparamos a los jóvenes para la economía global. Activamos a la juventd para los empleos del futuro.',
   },
   {
-    title: '',
-    info: '',
+    name: 'Endeavor',
+    description: 'Endeavor es una organización que desde hace más de 20 años lidera un movimiento global para detonar un mayor crecimiento económico, social y cultural a través de la selección, aceleración y promoción de los mejores Emprendedores de Alto Impacto. ',
   },
   {
-    title: '',
-    info: '',
+    name: 'Fundación EDUCA',
+    description: 'Trabajamos para que niños y jóvenes de escasos recursos en México continúen y concluyan sus estudios.',
   },
   {
-    title: '',
+    name: 'Laboratoria',
+    description: 'Laboratoria es una organización sin fines de lucro que recluta y capacita a mujeres talentosas que no han tenido acceso a oportunidades laborales y/o educación de calidad, brindándoles las habilidades y herramientas necesarias para encontrar un empleo en el sector tecnológico y desarrollar una carrera exitosa para su beneficio y el de su comunidad.',
   },
   {
-    title: '',
+    name: 'Vital Voices',
+    description: 'Vital Voices México es una asociación civil que promueve la participación y el desarrollo de la mujer como motor para el progreso de toda la sociedad. ',
   },
   {
-    title: '',
-  },
-  {
-    title: '',
-  },
-  {
-    title: '',
+    name: 'WEConnect International',
+    description: 'WEConnect International conecta a empresas pertenecientes a mujeres con corporativos multinacionales para integrarlas en las cadenas globales de valor, brindándoles además talleres y programas de capacitación para su desarrollo empresarial.',
   },
 ];
