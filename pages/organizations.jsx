9869b081cf2a8a2a3e5cfd1b2a5214e5b7dc6d19
AnaSalazar <anastephany.salazarguzman@gmail.com>
2020-02-13 04:22:05 -0600

    add page organizations

--- pages/organizations.jsx
+++ pages/organizations.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Link from 'next/link';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ongs from '../data/ongs';
 import NavigationBar from '../components/navigationBar';
 
 const LatinCodeWeek = () => (
   <div>
     <NavigationBar />
-    <div className="position">
-
-      Componente de Cards rectangulares
+    <div className="ongs">
+      { ongs.map(ong => (
+        <Link href={`/info-ong/?id=${ong.index}`} key={ong.title}>
+          <Paper elevation={3}>
+            <Grid container spacing={2} justify="center">
+              <Grid item xs={10} md={6}>
+                <img src={ong.logo} alt="" />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Typography className="title" variant="h4" gutterBottom>
+                  {ong.name}
+                </Typography>
+                <Typography className="title" gutterBottom>
+                  {ong.description}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Link>
+      ))}
     </div>
+    <style jsx>
+      {`
+        .ongs {
+          display: grid;
+          grid-gap: 25px;
+          padding: 15px;
+          padding-top: 110px;
+          align: center;
+        }
+        img {
+          border-radius: 3px;
+          width: 100%
+        }
+      `}
+    </style>
     <style jsx global>
       {`
         body {
@@ -15,13 +52,6 @@ const LatinCodeWeek = () => (
           font-family: system-ui;
           background: white;
         }
-        .position {
-          padding-top:95px;
-          max-width:1090px;
-          margin: 0 auto;
-
-        }
-        
       `}
     </style>
   </div>
