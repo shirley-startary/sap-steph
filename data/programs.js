6b0f043e697682ddce397b3346e79f9e690a438f
Ana Steph <anastephany.salazarguzman@gmail.com>
2020-02-13 06:15:44 -0600

    Page/info ong (#43)
    * load(ongs): loading data of ongs
    
    * add page organizations
    
    * add programs

--- data/programs.js
+++ data/programs.js
@@ -1,29 +1,110 @@
 export default [
   {
-    title: '',
-    info: '',
+    ong: 'Junior Achievement',
+    programs: [
+      {
+        program: 'Socios por un Día ',
+        image: '',
+        text: 'Un programa donde el participante experimenta un día laboral en una empresa y se proporcionan elementos de orientación vocacional. Más de 1,000 estudiantes beneficiados en México y Argentina.',
+        more: '',
+      },
+      {
+        program: 'Parque Financiero ',
+        image: '',
+        text: 'Más de 1,000 estudiantes han participado en este programa que introduce a jóvenes de educación media superior a la planeación económica, aprendiendo de forma práctica conceptos básicos de finanzas para el manejo de sus finanzas personales.',
+        more: '',
+      },
+      {
+        program: 'Ensamblando tu Carrera',
+        image: '',
+        text: 'Se proporcionan las habilidades blandas y técnicas para incorporarse al mundo laboral.',
+        more: '',
+      },
+      {
+        program: 'Competencias para la Vida',
+        image: '',
+        text: 'Desarrollar competencias sociales, emprendedoras y cardinales que faciliten la inserción laboral de los jóvenes, su desarrollo profesional y personal.',
+        more: '',
+      },
+      {
+        program: 'Foro Internacional de Emprendedores',
+        image: '',
+        text: 'Programa de una semana que tiene como objetivo desarrollar en los participantes actitudes y aptitudes emprendedoras, así como trabajar en el fortalecimiento de sus valores. Más de 1,300 jóvenes han sido impactados con el FIE.',
+        more: '',
+      },
+      {
+        program: 'Desafío Innova',
+        image: '',
+        text: 'Programa de alto rendimiento en el que los estudiantes se enfrentan al desafío de dar solución a una problemática social o ambiental.',
+        more: '',
+      },
+      {
+        program: 'Cuentas Contigo',
+        image: '',
+        text: 'Un programa de inclusión financiera donde a través de una aplicación se experimenta la importancia de tomar decisiones asertivas para desarrollar un presupuesto personal.',
+        more: '',
+      },
+    ],
   },
   {
-    title: '',
-    info: '',
+    ong: 'Endeavor',
+    programs: [
+      {
+        program: 'SAP SaaS Selection Powered by Endeavor',
+        image: '',
+        text: 'Con el objetivo fue descubrir a las próximas promesas en la vertical de Software as a Service (SaaS) se seleccionaron a cinco empresas emergentes a través de una rigurosa evaluación con criterios de innovación, escalabilidad, impacto y tracción, considerando que todas ellas tienen más de dos años en el mercado.',
+        more: '',
+      },
+    ],
   },
   {
-    title: '',
-    info: '',
+    ong: 'Fundación EDUCA',
+    programs: [
+      {
+        program: 'Congreso EDUCA',
+        image: '',
+        text: 'SAP México ha participado por seis años en el Congreso EDUCA, un espacio de aprendizaje y desarrollo para profesores que son parte de la RED EDUCA. En su edición 2019 asistieron más de 850 profesores, quienes participaron en conferencias magistrales y talleres simultáneos enfocados en el desarrollo del pensamiento lógico-matemático, educación ambiental y liderazgo.',
+        more: '',
+      },
+      {
+        program: 'Más que un regalo',
+        image: '',
+        text: 'Cada año los colaboradores de SAP México donan juguetes a niños de la RED EDUCA en el marco de Navidad y Día de Reyes. Esta actividad se lleva a cabo en Ciudad de México y Monterrey.',
+        more: '',
+      },
+    ],
   },
   {
-    title: '',
+    ong: 'Laboratoria',
+    programs: [
+      {
+        program: 'Diversidad e inclusión de género en la economía digital',
+        image: '',
+        text: 'Como parte de los compromisos de SAP México para contribuir a los ODS de Naciones Unidas, en 2019 se alió con Laboratoria para formar a la octava generación de desarrolladoras front-end y UX en la Ciudad de México. Fueron más de 40 mujeres quienes se capacitaron desde junio hasta noviembre de 2019 en Javascript, HTML, CSS, React y otras herramientas, complementando esta currícula con el desarrollo de habilidades blandas. A través de conferencias, talleres y mentorías, SAP México contribuyó con la participación de voluntarios a la formación de esta generación.',
+        more: '',
+      },
+    ],
   },
   {
-    title: '',
+    ong: 'Vital Voices',
+    programs: [
+      {
+        program: 'Programa de mentoría 2019',
+        image: '',
+        text: 'SAP México participó por primera vez en el programa anual de Mentoría para Empresarias y Emprendedoras de Vital Voices México con el objetivo desarrollar y fortalecer a emprendedoras a través de una relación de mentoría de nueve meses. Este proyecto benefició de manera directa a 20 mujeres propietarias de sus negocios o emprendimientos y tuvo la participación de 21 mentores y mentoras de corporativos como SAP México. Los logros obtenidos por las parejas participantes durante el proceso de mentoría se centraron en cuatro áreas: incubación, reestructuración, expansión e internacionalización y nuevas líneas de negocio.  ',
+        more: '',
+      },
+    ],
   },
   {
-    title: '',
-  },
-  {
-    title: '',
-  },
-  {
-    title: '',
+    ong: 'WEConnect International',
+    programs: [
+      {
+        program: 'Educación digital para la inclusión de mujeres en la economía',
+        image: '',
+        text: 'SAP México y WEConnect International trabajan en conjunto para acelerar la adopción digital de pymes lideradas por mujeres, ayudándolas a ser más competitivas, mejorar la calidad de los empleos que generan e innovar en sus negocios para integrarse a las cadenas de valor y así, impulsar el desarrollo económico del país. Como parte de esta iniciativa, se lanzó el programa Herramientas digitales en la Economía de la Experiencia a través del cual se capacitaron durante 8 sesiones a 25 empresarias mexicanas en habilidades y tecnologías emergentes como Machine Learning, Internet de las Cosas, Analíticos, etc., acercándolas a la actual economía digital y su impacto en los negocios.',
+        more: '',
+      },
+    ],
   },
 ];
--- pages/info-ong.jsx
+++ pages/info-ong.jsx
@@ -1,25 +1,116 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import NavigationBar from '../components/navigationBar';
+import programsOng from '../data/programs';
 
-const infoOng = () => (
-  <div>
-    <NavigationBar />
-    <div className='position'>
-      Info de las organizaciones
-    </div>
-    <style jsx global>
-      {`
-        body {
-          margin: 0;
-          font-family: system-ui;
-          background: white;
-        }
-        .position {
-          padding: 100px;
-        }
-      `}
-    </style>
-  </div>
-);
 
-export default infoOng;
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingTop: '120px',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+}));
+
+const InfoONG = (props) => {
+  const [expanded, setExpanded] = React.useState(false);
+  const { programs } = props;
+  const classes = useStyles();
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <>
+      <NavigationBar />
+
+      { programs.programs.map(program => (
+        <Card className={classes.root} elevation={3}>
+          <CardHeader
+            title={program.program}
+            subheader="September 14, 2016"
+          />
+          <CardMedia
+            className={classes.media}
+            image="/static/images/cards/paella.jpg"
+            title="Paella dish"
+          />
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              This impressive paella is a perfect party dish and a fun meal to
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>Method:</Typography>
+              <Typography paragraph>
+                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                minutes.
+              </Typography>
+              <Typography paragraph>
+                remaining 4 1/2 cups chicken broth; bring to a boil.
+              </Typography>
+              <Typography paragraph>
+                again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                minutes more. (Discard any mussels that don’t open.)
+              </Typography>
+              <Typography>
+                Set aside off of the heat to let rest for 10 minutes, and then serve.
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+      ))}
+    </>
+  );
+};
+
+InfoONG.getInitialProps = async ({ query }) => {
+  const programs = await programsOng[parseInt(query.id, 10) - 1];
+  console.log(programs.programs);
+  return { programs };
+};
+
+export default InfoONG;
