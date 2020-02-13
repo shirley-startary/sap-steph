6b47cef92c527b9febf201a32698c9fe0b043bd6
AnaSalazar <anastephany.salazarguzman@gmail.com>
2020-02-13 04:22:05 -0600

    add page organizations

--- data/ongs.js
+++ data/ongs.js
@@ -1,26 +1,38 @@
 export default [
   {
+    index: 1,
     name: 'Junior Achievement',
     description: 'Inspiramos y preparamos a los jóvenes para la economía global. Activamos a la juventd para los empleos del futuro.',
+    logo: 'https://user-images.githubusercontent.com/25916511/74423581-224dea00-4e16-11ea-9ca3-850972db8948.png',
   },
   {
+    index: 2,
     name: 'Endeavor',
     description: 'Endeavor es una organización que desde hace más de 20 años lidera un movimiento global para detonar un mayor crecimiento económico, social y cultural a través de la selección, aceleración y promoción de los mejores Emprendedores de Alto Impacto. ',
+    logo: 'https://user-images.githubusercontent.com/25916511/74424074-ec5d3580-4e16-11ea-8b63-98f8c844a300.png',
   },
   {
+    index: 3,
     name: 'Fundación EDUCA',
     description: 'Trabajamos para que niños y jóvenes de escasos recursos en México continúen y concluyan sus estudios.',
+    logo: 'https://user-images.githubusercontent.com/25916511/74423024-31806800-4e15-11ea-99e3-3d0a43f97d01.png',
   },
   {
+    index: 4,
     name: 'Laboratoria',
     description: 'Laboratoria es una organización sin fines de lucro que recluta y capacita a mujeres talentosas que no han tenido acceso a oportunidades laborales y/o educación de calidad, brindándoles las habilidades y herramientas necesarias para encontrar un empleo en el sector tecnológico y desarrollar una carrera exitosa para su beneficio y el de su comunidad.',
+    logo: 'https://user-images.githubusercontent.com/25916511/74423894-9e483200-4e16-11ea-955c-f4924357618a.png',
   },
   {
+    index: 5,
     name: 'Vital Voices',
     description: 'Vital Voices México es una asociación civil que promueve la participación y el desarrollo de la mujer como motor para el progreso de toda la sociedad. ',
+    logo: 'https://user-images.githubusercontent.com/25916511/74423371-c2efda00-4e15-11ea-85d1-109919f8fdf0.png',
   },
   {
+    index: 6,
     name: 'WEConnect International',
     description: 'WEConnect International conecta a empresas pertenecientes a mujeres con corporativos multinacionales para integrarlas en las cadenas globales de valor, brindándoles además talleres y programas de capacitación para su desarrollo empresarial.',
+    logo: 'https://user-images.githubusercontent.com/25916511/74423430-e155d580-4e15-11ea-8fbf-004692188546.png',
   },
 ];
